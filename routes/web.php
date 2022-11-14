9bfdc1ba272f079ef6787f03a71b0a61ce063b31
tomoyahiroe <tomoya4creative@gmail.com>
2022-11-14 16:21:33 +0900

    feat: /find page

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [AuthorController::class, 'index']);
+
+Route::get('/find', [AuthorController::class, 'find']);
+Route::post('/find', [AuthorController::class, 'search']);
